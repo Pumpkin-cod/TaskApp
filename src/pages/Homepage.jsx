01aa47e5bb127e4eee0aa41bdb799c2932c332ac
Catherine <katieboye44@gmail.com>
2025-06-04 11:46:12 +0000

    first commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hompage = () => {
+  return (
+    <div>
+      hello world
+      <p>Welcome to the homepage!</p>
+      <p>This is a simple React application.</p>
+    </div>
+  )
+}
+
+export default Hompage
